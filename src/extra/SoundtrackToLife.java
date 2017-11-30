3c5da5d3e24b7bb9493a48dbfaf241d612d06fc7
TJHL <league@iMac.attlocal.net>
2017-11-29 18:35:58 -0800

    Nasa Soundtrack Stringmethods 1 and 2

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -9,17 +9,36 @@ import javax.swing.JOptionPane;
 public class SoundtrackToLife {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "What is your mood", "Ur Mood", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "happy", "sad", "angry" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+		System.out.println(userMood);
+		
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
 		
+if(userMood==0) {
 
-		
+	playVideo("http://bit.ly/video-for-happy");
+	
+}
+if(userMood==1) {
+
+	playVideo("http://bit.ly/video-for-sad");
+	
+}
+if(userMood==2) {
+
+	playVideo("http://bit.ly/video-for-angry");
+	
+}
+	
 		// 4. Play different songs for other moods.
 
 /**
--- src/extra/StringMethods2.java
+++ src/extra/StringMethods2.java
@@ -4,20 +4,25 @@ package extra;
 
 public class StringMethods2 {
 	public static void main(String[] args) {
-		
+	
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
-
+String d = "FGHKggbuOGuyg";
+int	end = d.length();
+int start = end-3;
 		// 2. Print your String to the console in upper case.
-		
+		System.out.println(d.toUpperCase());
 		// 3. Print your String to the console in lower case.
-
+		System.out.println(d.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
+		System.out.println(d.substring(0,3));
+		
+		System.out.println(d.substring(9,10));
 		
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(d.substring(start,end));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
 	}
